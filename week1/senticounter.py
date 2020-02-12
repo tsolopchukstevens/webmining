b49493b41501f66ffcdce62dd2ba2028c4c0a0cf
ccorrado <ccorrads@gmail.com>
2020-02-11 19:44:11 -0500

    Solved checking for neg words between the and phone.

--- week1/senticounter.py
+++ week1/senticounter.py
@@ -23,8 +23,22 @@ def run(input_file_path):
     for phrase in phrases:
         for word in neg_words:
             if word in phrase:
-                # TODO only count the negative words in the input file between "the" and "phone"
-                found_dict[word] += 1
+                phrase_iterator = iter(phrase.split(' '))
+                done_looping = False
+                while not done_looping:
+                    try:
+                        item = next(phrase_iterator)
+                        if item in neg_words:
+                            found_dict[item] += 0
+                        if item == "the":
+                            next_item = next(phrase_iterator)
+                            if next_item in neg_words:
+                                found_dict[next_item] += 0
+                                word_after_neg = next(phrase_iterator)
+                                if word_after_neg == "phone":
+                                    found_dict[next_item] += 1
+                    except StopIteration:
+                        done_looping = True
     return found_dict
 
 
